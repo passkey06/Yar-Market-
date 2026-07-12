9d36a66a4fd3c0a7dab8d9cbc7276ceb2193920b
passkey06 <passkey1@gmail.com>
2026-07-12 13:55:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Yarış Market JavaScript
+const whatsappNo = "905538577094";
+
+const deliveryFee = 50;
+
+function startShopping() {
+    document.querySelector(".categories").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+document.querySelector(".btn").addEventListener("click", startShopping);
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        const title = card.querySelector("h3").innerText;
+
+        alert(title + " kategorisi çok yakında açılıyor.");
+
+    });
+
+});
+
+function orderWhatsApp(){
+
+let text="Merhaba, Yarış Market'ten sipariş vermek istiyorum.";
+
+window.open(
+`https://wa.me/${whatsappNo}?text=${encodeURIComponent(text)}`,
+"_blank"
+);
+
+}
